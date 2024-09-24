df85f451c6943b5a21db25ab4baa1bd96f2b8cab
esmameral <esma.meral@gmail.com>
2024-09-24 17:43:21 +0300

    first project

---
+++ FirstProject/src/ozyegin/model/Address.java
@@ -0,0 +1,8 @@
+package ozyegin.model;
+
+class Address {
+
+	public static void main(String[] args) {
+	}
+
+}
---
+++ FirstProject/src/ozyegin/model/Hello.java
@@ -0,0 +1,24 @@
+package ozyegin.model;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello " + "Java " + "World");
+		System.out.println("This is first day!!");
+		String schoolNumber = "S029175";
+		int grade1 = 85;
+		double PI = 3.14;
+		int i = 0;
+		for (i = 1; i <= 10; i++) {
+
+			System.out.println(i);
+		}
+
+	}
+
+	public static void read() {
+		System.out.println("example");
+
+	}
+
+}
