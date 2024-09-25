56dac5c5651d44988fad38d0f0d046f444c1aa3d
esmameral <esma.meral@gmail.com>
2024-09-25 16:17:22 +0300

    Week1/question1

---
+++ FirstProject/src/exercises/week1/Question1.java
@@ -0,0 +1,18 @@
+package exercises.week1;
+
+/*
+ * Write a java program that calculates the average of three 
+ * integers. Be careful about int division.
+ */
+public class Question1 {
+	public static void main(String[] args) {
+       int a=10;
+       int b=12;
+       int c=1;
+       double avg=(a+b+c)/3.0;
+       System.out.print("Average:"+avg);
+
+      
+	}
+
+}
