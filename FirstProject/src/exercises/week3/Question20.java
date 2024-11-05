eb1addb816ee02ef1e82865dfa1b737b94a5bdbc
esmameral <esma.meral@gmail.com>
2024-11-05 17:21:46 +0300

    Week3-Exercise20

---
+++ FirstProject/src/exercises/week3/Question20.java
@@ -0,0 +1,42 @@
+package exercises.week3;
+/**
+ * How to find all pairs on integer array whose sum is equal to given number
+ */
+public class Question20 {
+
+	public static void main(String[] args) {
+		int[] numbers= {1,4,3,5,9,2,0};
+		int value=5;
+		
+		String[] arr=findPairs(numbers,value);
+		for(String aPair:arr) {
+			System.out.print(aPair);
+		}
+
+	}
+	
+	public static String[] findPairs(int[] numbers, int value) {
+		int count=0;
+		for(int i=0;i<numbers.length-1;i++) {
+			for(int j=i+1;j<numbers.length;j++) {
+				if(numbers[i]+numbers[j]==value)
+					count++;
+			}
+		}
+		String[] pairs=new String[count];
+		int pairIndex=0;
+		for(int i=0;i<numbers.length-1;i++) {
+			for(int j=i+1;j<numbers.length;j++) {
+				if(numbers[i]+numbers[j]==value) {
+					pairs[pairIndex] ="["+numbers[i]+","+numbers[j]+"]";
+					pairIndex++;
+				}
+			}
+		}
+		return pairs;
+	}
+	
+	
+	
+
+}
